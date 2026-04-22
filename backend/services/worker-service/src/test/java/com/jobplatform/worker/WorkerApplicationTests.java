c6be53ff0eef5326dd4aad27747b494a1f6f46a2
ErErdem <ersinerdem5891@gmail.com>
2026-04-22 20:41:51 +0200

    worker service: make it running with empty structures and health endpoint

---
+++ backend/services/worker-service/src/test/java/com/jobplatform/worker/WorkerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jobplatform.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
